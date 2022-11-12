9efeb17efa9185394c240d4e2ecc6f74594576ee
IKJirawat <ik_jirawat@hotmail.com>
2022-11-13 00:54:12 +0700

    Rune parameter && print type: %T

--- main.go
+++ main.go
@@ -3,10 +3,18 @@ package main
 import "fmt"
 
 func main() {
+	var aint = 128567
+	var bfloat = 70.43
+	var r rune = 'F'
+
 	test := `just testing ei ei
 	nahhh
 	test not di farkkkk
 	eieiei
 	eiei`
 	fmt.Println(test)
+
+	fmt.Println(r)
+	fmt.Printf("Rune: %c \t Type: %T\n", aint, aint)
+	fmt.Printf("Rune: %.3f \t Type: %T\n", bfloat, bfloat)
 }
