2742b568ea0cb3d3978b72eaf5cd205ab5d76698
IKJirawat <ik_jirawat@hotmail.com>
2022-11-13 16:48:50 +0700

    Just printf ??

--- main.go
+++ main.go
@@ -7,14 +7,23 @@ func main() {
 	var bfloat = 70.43
 	var r rune = 'F'
 
+	fmt.Println(r)
+	fmt.Printf("Rune: %c \t Type: %T\n", aint, aint)
+	fmt.Printf("Rune: %.3f \t Type: %T\n", bfloat, bfloat)
+
+	// -------------------------------------------------------------//
+
 	test := `just testing ei ei
 	nahhh
 	test not di farkkkk
 	eieiei
 	eiei`
 	fmt.Println(test)
+	// -------------------------------------------------------------//
 
-	fmt.Println(r)
-	fmt.Printf("Rune: %c \t Type: %T\n", aint, aint)
-	fmt.Printf("Rune: %.3f \t Type: %T\n", bfloat, bfloat)
+	var title, year, rate, group, isSuperhero = "Avengers: Endgame", 2019, 8.4, "Sci-Fi", true
+	var fav rune = '⭐'
+
+	fmt.Printf(" เรื่อง: %s\n ปี: %d\n เรตติ้ง: %.1f\n ประเภท: %s\n ซุปเปอร์ฮีโร่: %t\n เรื่องโปรด: %c", title, year, rate, group, isSuperhero, fav)
+
 }
