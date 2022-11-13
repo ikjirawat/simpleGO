610972b7b43c174ed31220162ca0a25c117be98a
IKJirawat <ik_jirawat@hotmail.com>
2022-11-13 18:39:07 +0700

    Simple function in GO

--- main.go
+++ main.go
@@ -2,30 +2,59 @@ package main
 
 import "fmt"
 
+func nah() {
+	fmt.Println("Nah Func")
+}
+
 func main() {
-	var aint = 128567
-	var bfloat = 70.43
-	var r rune = 'F'
+	// var aint = 128567
+	// var bfloat = 70.43
+	// var r rune = 'F'
 
-	fmt.Println(r)
-	fmt.Printf("Rune: %c \t Type: %T\n", aint, aint)
-	fmt.Printf("Rune: %.3f \t Type: %T\n", bfloat, bfloat)
+	// fmt.Println(r)
+	// fmt.Printf("Rune: %c \t Type: %T\n", aint, aint)
+	// fmt.Printf("Rune: %.3f \t Type: %T\n", bfloat, bfloat)
+
+	// // -------------------------- Workshop 1 -----------------------------------//
+
+	// test := `just testing ei ei
+	// nahhh
+	// test not di farkkkk
+	// eieiei
+	// eiei`
+	// fmt.Println(test)
+
+	// // -------------------------- Workshop 2 -----------------------------------//
+
+	// var title, year, rate, genre, isSuperhero = "Avengers: Endgame", 2019, 8.4, "Sci-Fi", true
+	// var fav rune = '⭐'
+
+	// fmt.Printf(" เรื่อง: %s\n ปี: %d\n เรตติ้ง: %.1f\n ประเภท: %s\n ซุปเปอร์ฮีโร่: %t\n เรื่องโปรด: %c", title, year, rate, genre, isSuperhero, fav)
 
 	// -------------------------------------------------------------//
+	// ratings := 8.4
+	// if ratings < 5.0 {
+	// 	fmt.Println("Disappointed 😞")
+	// } else if ratings >= 5.0 && ratings < 7.0 {
+	// 	fmt.Println("Normal 😐")
+	// } else if ratings >= 7.0 && ratings < 10.0 {
+	// 	fmt.Println("Good 🥰")
+	// } else {
+	// 	fmt.Println("🤷🤷🤷🤷")
+	// }
 
-	test := `just testing ei ei
-	nahhh
-	test not di farkkkk
-	eieiei
-	eiei`
-	fmt.Println(test)
+	// ratings := 8.4
+	// switch {
+	// case ratings < 5.0:
+	// 	fmt.Println("Disappointed 😞")
+	// case ratings >= 5.0 && ratings < 7.0:
+	// 	fmt.Println("Normal 😐")
+	// case ratings >= 7.0 && ratings < 10.0:
+	// 	fmt.Println("Good 🥰")
+	// default:
+	// 	fmt.Println("🤷🤷🤷🤷")
+	// }
+	// -------------------------- Workshop 3 -----------------------------------//
 
-	// -------------------------------------------------------------//
-
-	var title, year, rate, genre, isSuperhero = "Avengers: Endgame", 2019, 8.4, "Sci-Fi", true
-	var fav rune = '⭐'
-
-	fmt.Printf(" เรื่อง: %s\n ปี: %d\n เรตติ้ง: %.1f\n ประเภท: %s\n ซุปเปอร์ฮีโร่: %t\n เรื่องโปรด: %c", title, year, rate, genre, isSuperhero, fav)
-
-	// -------------------------------------------------------------//
+	nah()
 }
